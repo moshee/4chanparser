b03224e83dcc05ed97561301ac0efbfebc82c1b6
moshee <teh.moshee@gmail.com>
2012-02-11 20:16:42 -0800

    moved stuff away from here

--- parse.rb
+++ parse.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'hpricot'
+require '4chan'
 
 def fmt(s)
   if s.size > 120 then return s[0..110] + '...' end
@@ -20,54 +21,7 @@ module Hpricot
   end
 end
 
-module FourChan
-  class Thread
-    def initialize(pic)
-      @pic = pic
-      @replies = []
-    end
-
-    def add_reply(reply)
-      @replies.push reply
-    end
-
-    attr_accessor :pic, :replies
-    attr_accessor :omitted_replies, :omitted_pics
-
-    def put
-      puts
-      print "\033[1mThread ##{@replies[0].id}\033[m"
-      print " - Not shown: #{@omitted_replies} replies" if @omitted_replies != nil
-      print " and #{@omitted_pics} pics" if @omitted_pics != nil
-      puts
-
-      @replies.each &:put
-    end
-  end
-
-  class Post
-    def initialize(id, is_op = false)
-      @id = id
-      @is_op = is_op
-    end
-
-    attr_accessor :poster_name, :body_text, :pic, :posted_on, :id
-
-    def put
-      if @is_op
-        print "  OP's post"
-      else
-        print "  Reply ##{@id}"
-      end
-      puts " by \033[36;1m#{@poster_name}\033[m"
-
-      puts "    Posted on #{@posted_on}"
-      puts "    Pic: #{@pic}" if @pic != nil
-      puts "    #{fmt(@body_text).inspect}"
-    end
-  end
-end
-
+t = Time.now
 doc = Hpricot(File.open 'page.html')
 delform = doc.at 'form[@name=delform]'
 threads = []
@@ -77,12 +31,16 @@ delform.each_child do |element|
   if element.elem?
     case
     when element['class'] == 'filesize' # <span class="filesize"> denotes beginning of thread lol
+      #context = element.following_siblings
       threads.push FourChan::Thread.new(element.at('a')['href'])
       current_thread = threads[-1]
 
     when (element['type'] == 'checkbox' and element['value'] == 'delete')
       current_thread.add_reply FourChan::Post.new(element['name'], true)
       current_post = current_thread.replies[-1]
+
+    when element['class'] == 'filetitle'
+      current_thread.title = element.inner_text
 
     when element['class'] == 'postername' # OP's post
       current_post.poster_name = element.inner_text
@@ -97,7 +55,24 @@ delform.each_child do |element|
       element.inner_text =~ /(\d+) posts (and (\d+) image)?/
       current_thread.omitted_replies = $1.to_i
       current_thread.omitted_pics = $3.to_i
-
+=begin
+    case
+    when element['class'] == 'filesize'
+      context = element.following_siblings
+      threads.push FourChan::Thread.new(element.at('a')['href'])
+      current_thread = threads[-1]
+      current_thread.add_reply FourChan::Post.new(context.at('input[value=delete]')['name'], true)
+      current_post = current_thread.replies[-1]
+      current_thread.title = context.at('.filetitle').inner_text
+      current_post.poster_name = context.at('.postername').inner_text
+      current_post.posted_on = context.at('.posttime').inner_text
+      current_post.body_text = context.at('blockquote').br2lf
+      if (omitted_posts = context.at('.omittedposts')) != nil
+        omitted_posts =~ /(\d+) posts (and (\d+) image)?/
+        current_thread.omitted_replies = $1.to_i
+        current_thread.omitted_pics = $3.to_i
+      end
+=end
     when element.name == 'table'
       if (reply = element.at('td.reply')) != nil # reply
         current_thread.add_reply FourChan::Post.new(reply['id'])
@@ -105,6 +80,7 @@ delform.each_child do |element|
 
         current_post.poster_name = reply.at('.commentpostername').inner_text
         current_post.posted_on = reply.inner_text[/\d\d\/\d\d\/\d\d\(\w+\)\d\d:\d\d/]
+        current_post.title = reply.at('.replytitle').inner_text
 
         if (text = element.at('blockquote')) != nil
           current_post.body_text = text.br2lf
@@ -115,4 +91,5 @@ delform.each_child do |element|
 end
 
 threads.each &:put
-#puts threads.inspect
+puts Time.now - t
+# case/when: 0.056s
